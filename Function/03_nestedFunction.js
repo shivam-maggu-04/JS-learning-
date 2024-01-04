140da7110682534463a8cfcd0acd97b7c627d610
shivam-maggu-04 <Shivammaggu31@gmail.com>
2024-01-05 00:26:07 +0530

    basic of funtion

---
+++ Function/03_nestedFunction.js
@@ -0,0 +1,13 @@
+function one(){
+    const userName = "Shivam";
+
+    function two()
+    {
+        const website = "Youtube";
+        console.log(userName);
+    }
+    // console.log(website);
+    two();
+}
+
+one();
